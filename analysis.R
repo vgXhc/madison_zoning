76d5774eff7fd52d7e27bca5a94a6c229af0da5c
vgXhc <kliems@gmail.com>
2019-06-23 18:35:46 -0500

    added analysis that isn't just comparing to residential districts but also industrial etc.

--- analysis.R
+++ analysis.R
@@ -30,12 +30,34 @@ sfr_zones <- c("SR-C1",
          "TR-C3", 
          "TR-R")
 
+# someone was interested what the analysis would look 
+# like if instead of looking at only residential, you 
+# would look at all land, except for parks etc. New variable
+# for all "Special Districts"
+other <- c("A",
+           "UA",
+           "CN",
+           "PR",
+           "AP",
+           "CI",
+           "PD",
+           "PHMP")
+
+#calculate total residential area
 res_total <- data %>%
   filter(ZONING_CODE %in% res_zones) %>%
   summarize(sum(ShapeSTArea))
 
+#detached single-family only
 res_sfr <- data %>%
   filter(ZONING_CODE %in% sfr_zones) %>%
   summarize(sum(ShapeSTArea))
 
+# only special district area
+other_area <- data %>%
+  filter(ZONING_CODE %in% other) %>%
+  summarise(sum(ShapeSTArea))
+
+total <- data %>% summarize(sum(ShapeSTArea))
+res_sfr/(total-other_area)
 sfr_ratio <- round((res_sfr/res_total)*100, digits = 0)
